61a97065073962886f05a24a47d2df3c88a66874
apcodes21 <98708815+apcodes21@users.noreply.github.com>
2024-11-13 18:22:44 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -181,13 +181,13 @@ with st.container():
                 }
             </style>
             """, unsafe_allow_html=True)
-              st.write("""
-                    1. Look at the swing GIF displayed above.<br>
-                    2. Start typing the name of the PGA player you think is performing the swing.<br>
-                    3. Suggestions will appear based on your input.<br>
-                    4. Click on a suggestion to make your guess.<br>
-                    5. After selecting a player, feedback will be provided (correct or incorrect).
-                """, unsafe_allow_html=True)
+          st.write("""
+                1. Look at the swing GIF displayed above.<br>
+                2. Start typing the name of the PGA player you think is performing the swing.<br>
+                3. Suggestions will appear based on your input.<br>
+                4. Click on a suggestion to make your guess.<br>
+                5. After selecting a player, feedback will be provided (correct or incorrect).
+            """, unsafe_allow_html=True)
         
         elif selected_tab == "Past Player List":
             st.subheader("Past Player List")
