ad2424c95f1ea1d8a1f6fb47490befc79d5e9a06
apcodes21 <98708815+apcodes21@users.noreply.github.com>
2024-11-13 17:18:55 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -210,7 +210,7 @@ with st.container():
 # Tab Content
 if selected_tab == "Home":
     # st.subheader("#1: 11/14/2024")
-    st.markdown('<h3 style="text-align: center; margin-top: 10px;">#1: 11/14/2024</h3>', unsafe_allow_html=True)
+    st.markdown('<h3 style="text-align: center; margin-top: 5px;">#1: 11/14/2024</h3>', unsafe_allow_html=True)
     # # Display the GIF of the player's swing (replace with an actual player's swing GIF)
     # st.video("https://vimeo.com/1029391107")  # Example GIF URL
     
