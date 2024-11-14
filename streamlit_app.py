b93ca5c980e0973f41795317e8dce01066a5f0eb
apcodes21 <98708815+apcodes21@users.noreply.github.com>
2024-11-13 18:52:46 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,12 +11,6 @@ with st.sidebar:
         icons=['house', 'gear'], menu_icon="cast", default_index=1)
     selected
 
-# 2. horizontal menu
-selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
-    menu_icon="cast", default_index=0, orientation="horizontal")
-selected2
-
 # Header Section
 st.markdown("""
     <style>
